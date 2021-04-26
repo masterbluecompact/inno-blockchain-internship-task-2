baaa9c96d25a202d9124dc36bd10de24357ef855
t0m <husamirami@gmail.com>
2021-04-26 23:10:03 +0300

    cleaned app.ts

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,34 +1,3 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-// import {RPC_URL_RINKEBY} from './constants'
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           {RPC_URL_RINKEBY}
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 import React from 'react'
 import { useWallet, UseWalletProvider } from 'use-wallet'
 import { usePlayerState } from './state/usePlayerState'
